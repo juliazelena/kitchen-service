c8a0f90b3929c93dcdd5182ce1ca75e2eff03daa
ulichka <ulichka7615@gmail.com>
2025-03-06 18:01:14 +0100

    create view index for home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, DishType, Cook
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
+    }
+    return render(request, "kitchen/index.html", context=context)
