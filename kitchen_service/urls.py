b4890a9df64d3a0b2acb3c01e5b499c3a085c1e5
ulichka <ulichka7615@gmail.com>
2025-03-12 13:30:31 +0100

    create path for login and logout

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from kitchen.views import UserLoginView, logout_view
+from kitchen_service import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("kitchen/", include("kitchen.urls", namespace="kitchen")),
-]
+    path("kitchen/",include("kitchen.urls", namespace="kitchen")),
+    path("accounts/login/", UserLoginView.as_view(), name="login"),
+    path("accounts/logout/", logout_view, name="logout")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
