3472eb368485eca49c21c73f77b5cea9642555ab
ulichka <ulichka7615@gmail.com>
2025-03-14 15:40:57 +0100

    add path dish-types/delete

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -5,18 +5,30 @@ from kitchen.views import (
     DishTypeListView,
     DishTypeCreateView,
     DishTypeUpdateView,
+    DishTypeDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
-    path("dish-types/create/", DishTypeCreateView.as_view(),
-         name="dish-type-create",
-         ),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create",
+    ),
     path(
         "dish-types/<int:pk>/update/",
         DishTypeUpdateView.as_view(),
         name="dish-type-update",
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete",
     ),
 ]
 
