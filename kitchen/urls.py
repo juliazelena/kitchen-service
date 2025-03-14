102a07b9632a7f8b6fe399bfdb019afb2f881bc3
ulichka <ulichka7615@gmail.com>
2025-03-14 14:52:55 +0100

    add path dish-types/create

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 
-from kitchen.views import index, DishTypeListView
+from kitchen.views import index, DishTypeListView, DessertTypeCreateView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list")
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-types/create/", DessertTypeCreateView.as_view(),
+        name="dessert-type-create",
+    ),
 ]
 
 app_name = "kitchen"
