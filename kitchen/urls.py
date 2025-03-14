893ce50ebfea02200dc805cb2d91aa81671a5d66
ulichka <ulichka7615@gmail.com>
2025-03-14 15:25:01 +0100

    add path dish-type/update

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
 
-
-from kitchen.views import index, DishTypeListView, DessertTypeCreateView
+from kitchen.views import (
+    index,
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
-    path("dish-types/create/", DessertTypeCreateView.as_view(),
-        name="dessert-type-create",
+    path("dish-types/create/", DishTypeCreateView.as_view(),
+         name="dish-type-create",
+         ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update",
     ),
 ]
 
