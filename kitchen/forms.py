1f1d34a2bdaf0b365a8200db97bd0981d3e4db97
ulichka <ulichka7615@gmail.com>
2025-03-14 12:11:06 +0100

    create dish type forms for search and creation

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
 from django.utils.translation import gettext_lazy as _
 
+from kitchen.models import DishType
+
 
 class UserLoginForm(AuthenticationForm):
     username = UsernameField(
@@ -30,3 +32,33 @@ class UserLoginForm(AuthenticationForm):
             attrs={"class": "form-check-input"}
         ),
     )
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Search by name",
+            }
+        ),
+    )
+
+class DishTypeForm(forms.ModelForm):
+    name = forms.CharField(
+        label=_("Name*:"),
+        max_length=255,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg mb-3",
+                "placeholder": "Name"
+            }
+        ),
+    )
+
+    class Meta:
+        model = DishType
+        fields = "__all__"
