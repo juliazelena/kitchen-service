0c69460dc7e5d25ff59d545691235f5388298c28
ulichka <ulichka7615@gmail.com>
2025-03-18 14:20:41 +0100

    add paths ingredients/,ingredients/create/, ingredients/<int:pk>/update/, ingredients/<int:pk>/delete/

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -49,6 +49,26 @@ urlpatterns = [
         DishCreateView.as_view(),
         name="dish-create",
     ),
+    path(
+        "ingredients/",
+        IngredientListView.as_view(),
+        name="ingredient-list"
+    ),
+    path(
+        "ingredients/create/",
+        IngredientCreateView.as_view(),
+        name="ingredient-create",
+    ),
+    path(
+        "ingredients/<int:pk>/update/",
+        IngredientUpdateView.as_view(),
+        name="ingredient-update",
+    ),
+    path(
+        "ingredients/<int:pk>/delete/",
+        IngredientDeleteView.as_view(),
+        name="ingredient-delete",
+    ),
 ]
 
 app_name = "kitchen"
