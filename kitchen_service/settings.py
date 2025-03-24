f9800c25302ad374475df54d69bce2686f46ccb8
ulichka <ulichka7615@gmail.com>
2025-03-24 15:46:59 +0100

    remove secret_key

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -11,18 +11,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-285*fp#5%aqeo&)tniuf@bps_g_axi^$ucw$x=0t(8y$r)ffee"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 ALLOWED_HOSTS = []
 
