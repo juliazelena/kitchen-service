56d7efeb92642ed04bdc5334f8254d1a6898fa06
ulichka <ulichka7615@gmail.com>
2025-04-15 21:07:12 +0200

    Add AWS S3 settings and storages app for media file handling

--- kitchen_service/settings/prod.py
+++ kitchen_service/settings/prod.py
@@ -30,3 +30,15 @@ DATABASES = {
         },
     }
 }
+
+INSTALLED_APPS += ["storages"]
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "my-kitchen-media"
+AWS_S3_REGION_NAME = "eu-central-1"
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
